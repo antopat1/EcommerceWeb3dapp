4da371a1dfcb71aacb8013419c6b84c82f7d7446
antopat1 <antopat1@gmail.com>
2025-04-14 19:37:03 +0200

    UpDateEnv

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -1,2 +1,2 @@
-export const projectId = "8e13e44fabd915f10275f83d71d37590";
-export const addressGianni = "0xd3351a5995585252d1d8bFE89157a309943be069";
+export const projectId = import.meta.env.VITE_PROJECT_ID;
+export const addressGianni = import.meta.env.VITE_ADDRESS_GIANNI;
